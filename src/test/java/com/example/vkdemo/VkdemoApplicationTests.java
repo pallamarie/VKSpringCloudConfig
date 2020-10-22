a776c0513ac8b2d1198a05329418519d26bbbebe
pallamarie <pallalentz@gmail.com>
2020-10-22 12:15:51 -0500

    Create VkdemoApplicationTests.java

---
+++ src/test/java/com/example/vkdemo/VkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
